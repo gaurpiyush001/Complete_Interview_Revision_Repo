cb0076f73d44c6be88fe9b4f987fea7fc4ead93e
gaurpiyush001 <gaurpiyush001@gmail.com>
2024-05-07 23:35:36 +0530

    sievevariations

---
+++ CompetitiveConcepts/Math/sieveVariations/MustPractice.cpp
@@ -0,0 +1,7 @@
+//https://www.hackerearth.com/problem/algorithm/monk-and-divisor-conundrum-56e0eb99/
+//bhai concept hai yaha ek sieve ka ki --> Sieve algo mein hum for Each number upto N, WE ITERATE OVER ITS MULTIPLE in N(logN) time.
+//PRECOMPUTE krna pdega vrna N^2 tk jaayegi Time Complexity
+//PRECOMPUTE krney ke liye intuition hai ki,, mujhe harr number ke liye jo given range hai P and Q ki,,,,usme harr ek ke liye kitney multiple present hai given vector mein
+
+
+//https://leetcode.com/problems/count-primes/description/
